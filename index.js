2ff2995cc6e9c50fe6f7aa6c60f8add60fb98345
gcyedhuijok <msjcaazi.khalid@hotmail.com>
2024-05-14 07:45:41 +0700

    demo task

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const dropRightWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[arr.length - 1])) arr = arr.slice(0, -1);
-  return arr;
-};
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+}
